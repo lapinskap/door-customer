c9ceed17c961558fef309e918f3f200f2fd2ae59
lapinskap <cherry3475@gmail.com>
2019-06-14 15:08:51 +0200

    initial commit

---
+++ build/precache-manifest.05dce027fcc1f2757453edaac0adfa36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6cd869a8b191c77c11890c5f1ec433a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cd522238c9efe196a60",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5ba01d91565a99b91fe1",
+    "url": "/static/css/main.e403af1f.chunk.css"
+  },
+  {
+    "revision": "4cd522238c9efe196a60",
+    "url": "/static/js/2.720f6aa8.chunk.js"
+  },
+  {
+    "revision": "5ba01d91565a99b91fe1",
+    "url": "/static/js/main.f88ec69a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
