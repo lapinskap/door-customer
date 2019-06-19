863a984b500da1b3b8a356665cc8e2a490fe34cf
lapinskap <cherry3475@gmail.com>
2019-06-19 11:49:46 +0200

    change navbar layout

--- src/components/menu/Navbar.js
+++ src/components/menu/Navbar.js
@@ -6,38 +6,37 @@ import {
   NavItem,
   NavLink,
   DropdownToggle,
+  UncontrolledButtonDropdown,
   DropdownMenu,
   DropdownItem,
-  UncontrolledButtonDropdown,
-  UncontrolledDropdown,
   Row,
   Container,
   Dropdown
 } from "reactstrap";
 import styled from "styled-components";
+import FaCog from "react-icons/lib/fa/cog";
+import MdPageview from "react-icons/lib/md/pageview";
 import FaPowerOff from "react-icons/lib/fa/power-off";
 
 export default class RenderNavbar extends React.Component {
   constructor(props) {
     super(props);
     this.handleOnLoad = this.handleOnLoad.bind(this);
+    this.toggle = this.toggle.bind(this);
     this.state = {
-
-        id: 1,
-        name: "Weissnat-Ward",
-        email: "kareybruen@vandervortkoepp.net",
-        phone_number: "1-228-585-5990",
-        address_line_1: "3697 Huey Summit",
-        address_line_2: "Apt. 399",
-        city: "North Charlineberg",
-        postal_code: "96032-0855"
-
+      dropdownOpen: false
     };
   }
 
   logOut() {
     window.localStorage.isAuthenticated = false;
     window.sessionStorage.isAuthenticated = false;
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
   }
 
   handleOnLoad = async event => {
@@ -57,41 +56,53 @@ export default class RenderNavbar extends React.Component {
     } catch (e) {
       alert(e.message);
     }
-  }
+}
 
   render() {
     return (
-      <Navbar expand="md" style={{ padding: "0" }} onLoad={this.handleOnLoad}>
+      <Navbar expand="md" style={{ padding: "0" }}>
         <Logo>
           <Media object src="image/logo.png" />
         </Logo>
+        <Container>
+          <Row>
+            <Nav navbar style={{ marginLeft: "110px" }}>
+            { window.sessionStorage.isAuthenticated === "true" ? <UncontrolledButtonDropdown>
+            <DropdownToggle caret size="lg">
+              My organization
+            </DropdownToggle>
+            <DropdownMenu>
+            <DropdownItem>
+              <i>{this.state.name}</i>
+              </DropdownItem>
+              <DropdownItem onClick={this.logOut} href={"/signin"}>
+              <FaPowerOff style={{ margin: "5px", marginBottom: "7px" }} />
+              Log out
+              </DropdownItem>
+            </DropdownMenu>
+              </UncontrolledButtonDropdown> : null}
+            </Nav>
+          </Row>
+        </Container>
 
-        <Select>
-          <UncontrolledDropdown className="select">
-          <DropdownToggle caret size="lg">
+        <UncontrolledDropdownX
+          inNavbar={true}
+          isOpen={this.state.dropdownOpen}
+          toggle={this.toggle}
+        >
+          <DropdownToggleX nav>
             Select Language
-          </DropdownToggle>
-          <DropdownMenu>
-            <DropdownItem>Polish</DropdownItem>
-            <DropdownItem>English</DropdownItem>
-          </DropdownMenu>
-        </UncontrolledDropdown>
-        </Select>
+          </DropdownToggleX>
 
-      { window.sessionStorage.isAuthenticated === "true" ? <UncontrolledButtonDropdown>
-        <DropdownToggle caret size="lg">
-          My organization
-        </DropdownToggle>
-        <DropdownMenu>
-        <DropdownItem>
-          <i>{this.state.name}</i>
-          </DropdownItem>
-          <DropdownItem onClick={this.logOut} href={"/signin"}>
-          <FaPowerOff style={{ margin: "5px", marginBottom: "7px" }} />
-          Log out
-          </DropdownItem>
-        </DropdownMenu>
-      </UncontrolledButtonDropdown> : null}
+          <DropdownMenuZ>
+            <DropdownItemY>
+              Polish
+            </DropdownItemY>
+            <DropdownItemY>
+              English
+            </DropdownItemY>
+          </DropdownMenuZ>
+        </UncontrolledDropdownX>
       </Navbar>
     );
   }
@@ -99,6 +110,22 @@ export default class RenderNavbar extends React.Component {
 
 const Logo = styled.div`
   padding: 20px 25px;
+`;
+
+const BottomText = styled(NavLink)`
+  color: #7387a9;
+  font-weight: 600;
+  font-size: 14px;
+`;
+
+const NavItems = styled(NavItem)`
+  padding: 12px 40px;
+  border-left: 2px solid #ebf2fa;
+  border-top: 2px solid #ebf2fa;
+  border-bottom: 2px solid #ebf2fa;
+  &:hover {
+    border-bottom: 2px solid #4ec2e2;
+  }
 `;
 
 const NavItemsEnd = styled(NavItem)`
@@ -109,42 +136,12 @@ const NavItemsEnd = styled(NavItem)`
   }
 `;
 
-const Select = styled.div`
-  .select {
-     background-color: #fff;
-    width: 400px;
-  font-weight: 500;
-  padding: 0px 20px;
-  border-left: 1px solid #eee;
-
-  padding: 12px 40px;
-  border: 2px solid #ebf2fa;
-  &:hover {
-    border-bottom: 2px solid #4ec2e2;
-
-    border-top: 0;
-  background-color: #fff;
-  width: 100%;
-  border-radius: 3px;
-  }
-
-  }
-
+const UncontrolledDropdownX = styled(Dropdown)`
   width: 400px;
   font-weight: 500;
   padding: 0px 20px;
   border-left: 1px solid #eee;
-  margin-left: 600px;
 `;
-
-const ControlledDropdown = styled(Dropdown)`
-  width: 400px;
-  font-weight: 500;
-  padding: 0px 20px;
-  border-left: 1px solid #eee;
-  margin-left: 600px;
-`;
-
 
 const DropdownMenuZ = styled(DropdownMenu)`
   border-top: 0;
@@ -164,3 +161,4 @@ const DropdownItemY = styled(DropdownItem)`
 const DropdownToggleX = styled(DropdownToggle)`
   color: #94a2bf;
 `;
+
